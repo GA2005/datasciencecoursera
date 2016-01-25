e0efd9812f809d49291c63e01b4f8f21223ec55f
GA2005 <roopa.ga@gmail.com>
2016-01-25 20:32:32 +0800

    Update Course3.R

--- Course3.R
+++ Course3.R
@@ -11,3 +11,9 @@ download.file(fileUrl, destfile = ".\\data\\jeff.jpg")
 img1<-readJPEG("jeff.jpg",TRUE)
 quantile(img1,probs=c(0.3,0.8))
 #error in jpeg corruption.
+
+#number 5
+quantile(mergedData$Gross.domestic.product.2012,probs=c(0.2,0.4,0.6,0.8,1))
+library(Hmisc)
+mergedData$gdp=cut2(mergedData$Gross.domestic.product.2012,g=5)
+table(mergedData$Income.Group,mergedData$gdp)
