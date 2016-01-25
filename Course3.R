ad68f86e09a7434e75824b8c3a9a57242a432f76
GA2005 <roopa.ga@gmail.com>
2016-01-25 14:57:48 +0800

    Update Course3.R

--- Course3.R
+++ Course3.R
@@ -3,3 +3,11 @@ download.file(fileUrl,dest".\data\hid.csv")
 dataf <- read.csv(".\\data\\hid.csv")
 agricultureLogical<- dataf$ACR==3 | dataf$AGS==6
 which(agricultureLogical)
+
+install.packages("jpeg")
+library(jpeg)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+download.file(fileUrl, destfile = ".\\data\\jeff.jpg")
+img1<-readJPEG("jeff.jpg",TRUE)
+quantile(img1,probs=c(0.3,0.8))
+#error in jpeg corruption.
